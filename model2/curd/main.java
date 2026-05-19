d52e14508d7e7172eda054e7f42a4c53eaae128e
Kishorp28 <kishore28k50@gmail.com>
2026-05-19 15:24:05 +0530

    Update main.java

--- model2/curd/main.java
+++ model2/curd/main.java
@@ -1,15 +1,4 @@
-package com.example.sqdb;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button bt1,bt2,bt3,bt4;
