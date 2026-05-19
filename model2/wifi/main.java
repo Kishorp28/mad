8e62f3ee3a9d8a36ee52f56642bfbad5c284e3c7
Kishorp28 <kishore28k50@gmail.com>
2026-05-19 15:14:22 +0530

    Update main.java

--- model2/wifi/main.java
+++ model2/wifi/main.java
@@ -1,11 +1,4 @@
-package com.example.wifi;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button1,button2;
