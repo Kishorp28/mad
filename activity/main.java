b395ca4711209c1fe994bc2965d0da7112782c81
Kishorp28 <kishore28k50@gmail.com>
2026-05-19 12:20:38 +0530

    Update main.java

--- activity/main.java
+++ activity/main.java
@@ -1,11 +1,3 @@
-package com.example.intent;
-
-import android.support.v7.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText etMessage;
