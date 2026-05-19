f378879349e90e398e0c76a710b0bc962b247026
Kishorp28 <kishore28k50@gmail.com>
2026-05-19 14:26:54 +0530

    Update main.java

--- thread/main.java
+++ thread/main.java
@@ -1,11 +1,4 @@
-package com.example.threads2;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
